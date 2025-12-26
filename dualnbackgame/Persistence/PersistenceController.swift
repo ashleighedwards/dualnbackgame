c449a93b0ad3002ee4ed8e566f374571bcf2a8bb
Ashleigh Edwards <ashleigh.edwards@mhr.co.uk>
2025-12-26 20:52:23 +0000

    Improve persistence controller

---
+++ dualnbackgame/Persistence/PersistenceController.swift
@@ -0,0 +1,42 @@
+
+//
+//  Persistence.swift
+//  dualnbackgame
+//
+//  Created by Ashleigh Edwards on 24/12/2025.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    @MainActor
+    static let preview: PersistenceController = {
+        let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+        return result
+    }()
+
+    let container: NSPersistentCloudKitContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "dualnbackgame")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                print("Core Data error: \(error)")
+                print(error.userInfo)
+            }
+        })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
